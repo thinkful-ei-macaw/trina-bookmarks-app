34b19a6db202e21a15643591a77fa68cfc43d15a
Trina <trinamceary@gmail.com>
2020-02-25 10:11:01 -0800

    complete api functions

--- src/api.js
+++ src/api.js
@@ -1,29 +1,64 @@
 'use strict'
 //api methods in here!!
-
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/trina/bookmarks';
+
+  const apiFetch = function(...args){
+    let error;
+    return fetch(...args)
+      .then(result => {
+        if (!result.ok) {
+          error = {code: result.status};
+
+          if(!result.headers.get('content-type').includes('json')) {
+            error.message = result.statusText;
+            return Promise.reject(error);
+        }
+      }
+      return result.json();
+    })
+    .then(data => {
+      if (error){
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+  };
 
   //GET
   function getBookmarks(){
-    fetch(BASE_URL)
-      .then(response => response.json())
-      .then(responseJson => console.log(responseJson))
-      //now what can I do with responsJson???
-    }
+    console.log(apiFetch(BASE_URL))
+    return apiFetch(BASE_URL)
+    };
     getBookmarks();
 
-  //POST (add)
-  function addBookmarks(){
+  //POST 
+  function addBookmarks(name){
+    const newBookmark = JSON.stringify(name);
+    return apiFetch(BASE_URL, {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: newBookmark
+    });
+  };
 
-  }
   //PATCH (edit)
-  function editBookmarks(){
+  function editBookmarks(id, updateData){
+    const newData = JSON.stringify(updateData);
+    return apiFetch(`${BASE_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {'content-type': 'application/json'},
+      body: newData
+    });
+  };
 
-  }
+
   //DELETE (delete)
-  function deleteBookmarks(){
-
-  }
+  function deleteBookmarks(id){
+    return apiFetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE'
+    });
+  };
 
 
 export default {
